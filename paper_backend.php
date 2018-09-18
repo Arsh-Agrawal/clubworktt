93f35612120ada0b76ff0a0aaa6e4efda30e60f7
Arsh-Agrawal <arsh.167.agrawal@gmail.com>
2018-09-18 20:12:07 +0530

    logout

--- paper_backend.php
+++ paper_backend.php
@@ -18,21 +18,21 @@
 		$user_ans=$_POST['ans'];
 		$q_no=$_POST['q_no'];
 		$sql='SELECT answer FROM exam WHERE id="'.$q_no.'"';
-		$correct_ans=mysql_query($connect,$sql);
+		$correct_ans=mysqli_query($connect,$sql);
 		if($user_ans==$correct_ans)
 		{
 			$inc='UPDATE user SET c_ans=c_ans+1 WHERE delegate ="'.$_SESSION['delegate'].'" '; 
-			$check=mysql_query($connect,$inc);
+			$check=mysqli_query($connect,$inc);
 			if($check)
 			{
 				$img_no=range(1,64);
 				shuffle($img_no);
 				$image='SELECT img FROM image WHERE id="'.$img_no'"';
 				//print the image
-				header("Location: exam.php");
+				
 			}
 			
-			$message="correct ans";
+			$message="correct answer";
 		}
 		else
 		{
@@ -40,5 +40,24 @@
 		}
 		unset($_POST['submit']);
 		$_SESSION['message']=$message;
+		header("Location: exam.php");
+	}
+	if(isset($_POST['final_submit']))
+	{
+		$sql='UPDATE logout SET over='true' WHERE delegate ="'.$_SESSION['delegate'].'" ';
+		$check=mysqli_query($connect,$sql);
+		if($check)
+		{
+			$message="successfully submition done";
+		}
+		else
+		{
+			$message="error in submition";
+		}
+
+		echo $message;
+		
+		// $_SESSION['final_message']=$message;
+		// header("Location: exam.php");
 	}
 ?>
