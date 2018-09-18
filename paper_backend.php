1a700f7ed37d9537955aa725cd58c4254a51d30d
Arsh-Agrawal <arsh.167.agrawal@gmail.com>
2018-09-18 19:59:04 +0530

    image output..checking ans..incrementing value and database management

--- paper_backend.php
+++ paper_backend.php
@@ -12,9 +12,8 @@
 		die('database connection failed');
 	}
 
-	unset($_SESSION['submit']);
-
-	if(isset($_SESSION['submit']))
+	
+	if(isset($_POST['submit']))
 	{
 		$user_ans=$_POST['ans'];
 		$q_no=$_POST['q_no'];
@@ -22,10 +21,24 @@
 		$correct_ans=mysql_query($connect,$sql);
 		if($user_ans==$correct_ans)
 		{
-			//show any one of the image
-			//use random function
+			$inc='UPDATE user SET c_ans=c_ans+1 WHERE delegate ="'.$_SESSION['delegate'].'" '; 
+			$check=mysql_query($connect,$inc);
+			if($check)
+			{
+				$img_no=range(1,64);
+				shuffle($img_no);
+				$image='SELECT img FROM image WHERE id="'.$img_no'"';
+				//print the image
+				header("Location: exam.php");
+			}
+			
+			$message="correct ans";
 		}
-		
+		else
+		{
+			$message="wrong answer";
+		}
+		unset($_POST['submit']);
+		$_SESSION['message']=$message;
 	}
-
 ?>
