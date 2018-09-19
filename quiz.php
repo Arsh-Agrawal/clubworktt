4debf94946bdc1169ddb4e371b64c4dc1b121e92
aditi-g21 <goyal.aditi121@gmail.com>
2018-09-19 20:16:26 +0530

    Partitioning the page

--- quiz.php
+++ quiz.php
@@ -1,23 +1,154 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Quiz</title>
+	<title>Exam</title>
 	<link rel="stylesheet" type="text/css" href="quiz.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet"> 
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300">
 </head>
 <body>
 
-	<form>
-		<div> 
-			<p> ur name? </p>
+	<div class="top">
+		<h1> Exam </h1>
+	</div>
 
-			<input type="radio" name="q1" value="a">
-			<input type="radio" name="q1" value="b">
-			<input type="radio" name="q1" value="c">
-			<input type="radio" name="q1" value="d">
-			<input type="submit" name="a1" value="submit">
-		</div>
+	<div class="container">
 
-	</form>
+		<div class="left">
+
+		<form>
+			<div> 
+				<p> ur name? </p>
+
+				<input type="radio" name="q1" value="a">
+				<input type="radio" name="q1" value="b">
+				<input type="radio" name="q1" value="c">
+				<input type="radio" name="q1" value="d">
+				<input type="submit" name="a1" value="submit">
+			</div>
+
+			<div> 
+				<p> ur name? </p>
+
+				<input type="radio" name="q1" value="a">
+				<input type="radio" name="q1" value="b">
+				<input type="radio" name="q1" value="c">
+				<input type="radio" name="q1" value="d">
+				<input type="submit" name="a1" value="submit">
+			</div>
+
+			<div> 
+				<p> ur name? </p>
+
+				<input type="radio" name="q1" value="a">
+				<input type="radio" name="q1" value="b">
+				<input type="radio" name="q1" value="c">
+				<input type="radio" name="q1" value="d">
+				<input type="submit" name="a1" value="submit">
+			</div>
+
+			<div> 
+				<p> ur name? </p>
+
+				<input type="radio" name="q1" value="a">
+				<input type="radio" name="q1" value="b">
+				<input type="radio" name="q1" value="c">
+				<input type="radio" name="q1" value="d">
+				<input type="submit" name="a1" value="submit">
+			</div>
+
+			<div> 
+				<p> ur name? </p>
+
+				<input type="radio" name="q1" value="a">
+				<input type="radio" name="q1" value="b">
+				<input type="radio" name="q1" value="c">
+				<input type="radio" name="q1" value="d">
+				<input type="submit" name="a1" value="submit">
+			</div>
+
+			<div> 
+				<p> ur name? </p>
+
+				<input type="radio" name="q1" value="a">
+				<input type="radio" name="q1" value="b">
+				<input type="radio" name="q1" value="c">
+				<input type="radio" name="q1" value="d">
+				<input type="submit" name="a1" value="submit">
+			</div>
+
+			<div> 
+				<p> ur name? </p>
+
+				<input type="radio" name="q1" value="a">
+				<input type="radio" name="q1" value="b">
+				<input type="radio" name="q1" value="c">
+				<input type="radio" name="q1" value="d">
+				<input type="submit" name="a1" value="submit">
+			</div>
+
+			<div> 
+				<p> ur name? </p>
+
+				<input type="radio" name="q1" value="a">
+				<input type="radio" name="q1" value="b">
+				<input type="radio" name="q1" value="c">
+				<input type="radio" name="q1" value="d">
+				<input type="submit" name="a1" value="submit">
+			</div>
+
+			<div> 
+				<p> ur name? </p>
+
+				<input type="radio" name="q1" value="a">
+				<input type="radio" name="q1" value="b">
+				<input type="radio" name="q1" value="c">
+				<input type="radio" name="q1" value="d">
+				<input type="submit" name="a1" value="submit">
+			</div>
+
+			<div> 
+				<p> ur name? </p>
+
+				<input type="radio" name="q1" value="a">
+				<input type="radio" name="q1" value="b">
+				<input type="radio" name="q1" value="c">
+				<input type="radio" name="q1" value="d">
+				<input type="submit" name="a1" value="submit">
+			</div>
+
+			<div> 
+				<p> ur name? </p>
+
+				<input type="radio" name="q1" value="a">
+				<input type="radio" name="q1" value="b">
+				<input type="radio" name="q1" value="c">
+				<input type="radio" name="q1" value="d">
+				<input type="submit" name="a1" value="submit">
+			</div>
+
+			<div> 
+				<p> ur name? </p>
+
+				<input type="radio" name="q1" value="a">
+				<input type="radio" name="q1" value="b">
+				<input type="radio" name="q1" value="c">
+				<input type="radio" name="q1" value="d">
+				<input type="submit" name="a1" value="submit">
+			</div>
+
+		</form>
+
+	</div>
+
+	<div class="right">
+		
+
+	</div>
+		
+	</div>
+	
 
 </body>
 </html>
