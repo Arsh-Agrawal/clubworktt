1f2872f4e5fa794471c295365b2544b4b1e2e600
Arsh-Agrawal <arsh.167.agrawal@gmail.com>
2018-10-03 00:02:09 +0530

    fucked

---
+++ index_back.php
@@ -0,0 +1,63 @@
+<?php
+	session_start();
+
+	$dbhost='localhost';
+	$dbuser='root';
+	$dbpass='';
+	$dbname='bmesi';
+	$connect=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+	if(mysqli_connect_errno())
+	{
+		die('database connection failed');
+	}
+
+	unset($reg,$name,$row,$numrows,$_SESSION['message'],$message,$_SESSION['reg']);
+
+	if($_POST['login'])
+	{
+		
+
+		$reg=$_POST['reg'];
+		$name=$_POST['name'];
+		$pass=$_POST['password'];
+
+		if(isset($reg,$name))
+		{
+
+			$check='SELECT * FROM login WHERE registration = "'.$reg.'"';
+			$r=mysqli_query($connect,$check);
+			$logout=mysqli_fetch_assoc($r);
+			$submition=$logout['over'];
+			if($submition==0)
+			{
+				//database update
+				
+				//login update
+				$over=1;  //cant login again with same registration number
+				$sql="INSERT INTO login (registration,name,over) VALUES ('$reg','$name','$over')";
+				$result=mysqli_query($connect,$sql);
+
+				//user answer update
+				$user_sql="INSERT INTO user(registration) VALUES ('$reg')";
+				mysqli_query($connect,$user_sql);
+			
+				// head to rules and regulation page
+				$_SESSION['rule']=0;
+				$_SESSION['reg']=$reg;
+				header("Location: rules1.php");
+			}
+			else
+			{
+				
+				$message="Paper already attempted";
+				$_SESSION['message']=$message;
+				header("Location: login1.php");
+			}
+		}
+		else
+		{
+			$_SESSION['message']="both the fields are required";
+			header("Location: login1.php");
+		}
+	}
+?>
--- login_back.php
+++ login_back.php
@@ -1,6 +1,5 @@
 <?php
 	session_start();
-
 	$dbhost='localhost';
 	$dbuser='root';
 	$dbpass='';
@@ -10,53 +9,76 @@
 	{
 		die('database connection failed');
 	}
-
-	unset($reg,$name,$row,$numrows,$_SESSION['message'],$message,$_SESSION['reg']);
-
+	
 	if($_POST['login'])
 	{
-		
-
+		unset($reg,$delegate,$row,$numrows,$_SESSION['message'],$message,$pass);
 		$reg=$_POST['reg'];
-		$name=$_POST['name'];
-
-		if(isset($reg,$name))
+		$pass=$_POST['password'];
+		if(isset($reg,$pass))
 		{
-
-			$check='SELECT * FROM login WHERE registration = "'.$reg.'"';
-			$r=mysqli_query($connect,$check);
-			$logout=mysqli_fetch_assoc($r);
-			$submition=$logout['over'];
-			if($submition==0)
-			{
-				//database update
-				
-				//login update
-				$over=1;  //cant login again with same registration number
-				$sql="INSERT INTO login (registration,name,over) VALUES ('$reg','$name','$over')";
-				$result=mysqli_query($connect,$sql);
-
-				//user answer update
-				$user_sql="INSERT INTO user(registration) VALUES ('$reg')";
-				mysqli_query($connect,$user_sql);
+			$sql='SELECT * FROM login WHERE registration = "'.$reg.'" AND name = "'.$pass.'"';
+			$row=mysqli_query($connect,$sql);
+			$numrows = mysqli_num_rows($row);
 			
-				// head to rules and regulation page
-				$_SESSION['rule']=0;
-				$_SESSION['reg']=$reg;
-				header("Location: rules.php");
+			unset($sql,$row);//can be used later too
+			if($numrows==1)
+			{
+				$check='SELECT * FROM login WHERE registration = "'.$reg.'"';
+				$r=mysqli_query($connect,$check);
+				
+				$logout=mysqli_fetch_assoc($r);
+				$submition=$logout['over'];
+				
+				if($submition==0)
+				{
+					//database update
+				
+					//login update
+					$over=1;  //cant login again with same registration number
+					$sql='UPDATE login SET over=1 WHERE registration ="'.$reg.'" ';
+					$result=mysqli_query($connect,$sql);
+
+					//user answer update
+					$user_sql="INSERT INTO user(registration) VALUES ('$reg')";
+					mysqli_query($connect,$user_sql);
+			
+					// head to rules and regulation page
+					$_SESSION['rule']=0;
+					$_SESSION['reg']=$reg;
+					header("Location: rules1.php");
+				}
+				else
+				{
+				
+					$message="Paper already attempted";
+					$_SESSION['message']=$message;
+					header("Location: login1.php");
+				}
 			}
 			else
 			{
+				$sql='SELECT * FROM login WHERE registration = "'.$reg.'"';
+				$connect_reg=mysqli_query($connect,$sql);
+				$chreg=mysqli_fetch_assoc($connect_reg);
+				$check_reg=$chreg['registration'];
 				
-				$message="Paper already attempted";
+				if($check_reg==$reg)
+				{
+					$message = "Wrong Password";
+					$_SESSION['reg'] = $reg; 
+				}
+				else
+					$message = "Wrong Registration Number";
+
 				$_SESSION['message']=$message;
-				header("Location: login.php");
+				header("Location: login1.php");
 			}
 		}
 		else
 		{
 			$_SESSION['message']="both the fields are required";
-			header("Location: login.php");
+			header("Location: login1.php");
 		}
 	}
 ?>
